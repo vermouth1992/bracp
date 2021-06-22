1a28d6302c5e144ce67a17dd2a18190517d12ed2
Chi Zhang <czhangseu@gmail.com>
2021-06-22 11:23:36 -0700

    update

--- copy_weights.py
+++ copy_weights.py
@@ -14,4 +14,17 @@ if __name__ == '__main__':
                             des_folder = f'env\={env}_reg\={reg}_use\={use}/env\={env}_reg\={reg}_use\={use}_s{seed}/'
                             command = 'cp ' + src_folder + file + ' ' + des_folder
                             # print(command)
-                            os.system(command)
+                            # os.system(command)
+
+    for env in env_names:
+        for reg in ['kl', 'mmd']:
+            for seed in [200, 201, 202]:
+                for file in ['policy_behavior_None_kl.ckpt.data-00000-of-00001',
+                             'policy_behavior_None_kl.ckpt.index',
+                             'policy_behavior_log_beta_None_kl.ckpt.data-00000-of-00001',
+                             'policy_behavior_log_beta_None_kl.ckpt.index']:
+                    src_folder = f'env\={env}_reg\={reg}_use\=true/env\={env}_reg\=kl_use\=true_s{seed}/'
+                    des_folder = f'env\={env}_reg\={reg}_use\=false/env\={env}_reg\={reg}_use\=false_s{seed}/'
+                    command = 'cp ' + des_folder + file + ' ' + src_folder
+                    print(command)
+                    # os.system(command)
